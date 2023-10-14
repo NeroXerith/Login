553f78ac6785a099998b5e14dc78d08c8d29d322
NeroXerith <astar8820@gmail.com>
2023-10-14 21:38:46 +0800

    function for pop-up modal added

--- js/login.js
+++ js/login.js
@@ -1,5 +1,15 @@
-// List of Pre-defined accounts
-const users = [
+// Function to open the error modal
+function openErrorMessageModal() {
+    document.getElementById('error-modal').style.display = 'block';
+  }
+  
+  // Function to close the error modal
+  document.getElementById('close-error-modal').addEventListener('click', function() {
+    document.getElementById('error-modal').style.display = 'none';
+  });
+  
+  // List of Pre-defined accounts
+  const users = [
     ['test', 'test123'],
     ['admin', 'password']
   ];
@@ -7,8 +17,8 @@ const users = [
   // Get the HTML login form element by its ID and assign it to the `loginForm` variable.
   const loginForm = document.getElementById('login-form');
   
-  // Event listener to trigger when submit button is clicked in the form
-  loginForm.addEventListener('submit', function(event) {  
+  // Event listener to trigger when the submit button is clicked in the form
+  loginForm.addEventListener('submit', function(event) {
     // Prevent the default form submission behavior.
     event.preventDefault();
   
@@ -17,14 +27,25 @@ const users = [
     const password = loginForm.password.value;
   
     // Check if the username and password match the predefined account.
+    let isValid = false;
     for (const user of users) {
       if (username === user[0] && password === user[1]) {
         // Redirect to the index page if the username and password are correct.
         window.location.href = 'index.html';
-        return;
+        isValid = true;
+        break;
       }
     }
   
-    // Display an error message if the username and password are incorrect.
-    alert('Invalid username or password.');
+    if (!isValid) {
+      // Display the error message modal if the username and password are incorrect.
+      openErrorMessageModal();
+
+      // Display a custom error message in the modal.
+    document.getElementById('error-message').textContent = 'Invalid username or password.';
+      
+      // For debug purposes
+    console.log('Invalid username or password.');
+    }
   });
+  
